c57875e072ea6c0a6e4eca01191b6c14b22e7954
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-07 22:28:49 +0530

    added welcome message in Login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./Components/Navbar"
+import Login from "./Components/Login"
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Login />
     </>
   )
 }
---
+++ src/Components/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Login = () => {
+  return (
+    <>
+      <div className="Main_body">
+        <div className="Login_Form">
+          <h2>Welcome Back</h2>
+          <p>Please Enter Your Login Credentials</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
