54f377cdaebdea0c3a246976f7008da9f8a2d3e6
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-08 18:13:37 +0530

    Changed title

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
       <div className="Main_body">
         <div className="Login_Form">
           <h2>Welcome Back</h2>
-          <p>Please Enter Your Login Credentials</p>
+          <p> Please Enter Your Login Details </p>
           <input
             type="text"
             placeholder="Enter Address*"
