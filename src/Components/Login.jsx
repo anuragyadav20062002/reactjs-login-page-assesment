ca16b6de9f46f24bb948f8c348409a5c037b5a20
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-02-08 13:04:49 +0530

    added axios

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-direct-mutation-state */
+/* eslint-disable no-native-reassign */
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react"
+import { useState } from "react"
 
 const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const changeEvent = (e) => {
+    setData({ [e.target.name]: e.target.value })
+    console.log(e.target.value)
+  }
+
+  const submitData = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <>
       <div className="Main_body">
@@ -13,13 +31,21 @@ const Login = () => {
             type="text"
             placeholder="Enter Address*"
             className="input_address"
+            name="email"
+            onChange={changeEvent}
+            value={data.email}
           />
           <input
             type="password"
             placeholder="Enter Password*"
             className="input_password"
+            name="password"
+            onChange={changeEvent}
+            value={data.password}
           />
-          <button className="login_button">Login</button>
+          <button className="login_button" onClick={submitData}>
+            Login
+          </button>
         </div>
         <div className="checkbox_items">
           <input type="checkbox" className="checkBox" />
